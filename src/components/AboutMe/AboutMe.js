61555503bc21a4ee6e33e26916d4fa0a45221b0e
Andrew K <nscapital08@gmail.com>
2022-11-02 00:58:32 +0300

    refactor

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,30 +3,37 @@ import profileFoto from '../../images/profile-foto.jpg'
 import arrow from '../../images/arrow.svg'
 
 function AboutMe() {
-     
-      
+
+    const date = new Date();
+    const dateOfBirth = '1983-8-23';
+    const dateOfBirthInMS = Date.parse(dateOfBirth);
+    const whatIsMyAgeAgain = Math.floor((date - dateOfBirthInMS) / (1000 * 3600 * 24 * 365));
+
+
     return (
         <section id='about-me'>
           <h2 className='section__catchword'>Студент</h2>
           <div className='about-me__profile-info'>
                 <div className='profile-info__description'>
                     <h3 className='profile-info__name'>Андрей</h3>
-                    <h4 className='profile-info__profession'>Фронтенд-разработчик, 38 лет</h4>
-                    <p className='profile-info__bio'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
-                      и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
-                      После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+                    <h4 className='profile-info__profession'>Фронтенд-разработчик, {whatIsMyAgeAgain} лет</h4>
+                    <p className='profile-info__bio'>Я&nbsp;начинающий веб-разработчик. Еще во&nbsp;времена AOL, Yahoo и&nbsp;narod.ru
+                        увлекался созданием простейших сайтов и&nbsp;вот, наконец, больше не&nbsp;могу сопротивляться желанию создавать и&nbsp;улучшать веб приложения,
+                        пришло время заняться этим серьезно. Большую часть свободного времени
+                        совершенствую свои навыки, учусь новому, впитываю информацию.
+                        Ещё люблю играть в&nbsp;баскетбол и&nbsp;катать на&nbsp;своих велосипедах, ходить в&nbsp;велопоходы.
                     </p>
                     <a href='https://github.com/mczoom' className='profile-info__contact-link link' target='_blank'>Github</a>
                 </div>
                 <img src={profileFoto} className='profile-info__foto' alt='Моё фото'></img>
-          </div>          
+          </div>
           <div className='about-me__portfolio'>
               <h4 className='portfolio__header'>Портфолио</h4>
               <ul className='about-me__portfolio-projects list'>
                 <li>
                   <a href='https://github.com/mczoom/how-to-learn' className='portfolio__project' target='_blank'>
                     <span >Статичный сайт</span>
-                    <div className='portfolio__link-icon'></div>                    
+                    <div className='portfolio__link-icon'></div>
                   </a>
                 </li>
                 <li>
@@ -42,7 +49,7 @@ function AboutMe() {
                   </a>
                 </li>
               </ul>
-          </div>          
+          </div>
         </section>
     );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 
 function Footer() {
-     
-      
+
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className='footer'>
           <h3 className='footer__header footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
           <div className='footer__divider'></div>
           <div className='footer__wrap'>
-              <p className='footer__copyright footer__text'>&copy; 2022</p>
+              <p className='footer__copyright footer__text'>&copy; {currentYear === 2022 ? currentYear : `2022 - ${currentYear}`}</p>
               <ul className='footer__links links footer__text'>
-                  <li className='links__link'><a href='https://practicum.yandex.ru/' className='link' target='_blank'>Яндекс.Практикум</a></li>
-                  <li className='links__link'><a href='https://github.com/mczoom' className='link' target='_blank'>Github</a></li>
+                  <li className='links__link'><a href='https://practicum.yandex.ru/' className='link' target='_blank' rel="noreferrer">Яндекс.Практикум</a></li>
+                  <li className='links__link'><a href='https://github.com/mczoom' className='link' target='_blank' rel="noreferrer">Github</a></li>
               </ul>
           </div>
         </footer>
